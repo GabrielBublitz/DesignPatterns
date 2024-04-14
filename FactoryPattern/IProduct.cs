e78d475a6b18a1677a54a2e8b875da1d6c69e7dc
Gabriel Henrique Mota Bublitz <gabriel.bublitz@hotmail.com>
2024-04-14 08:33:47 -0300

    Factory Pattern

---
+++ FactoryPattern/IProduct.cs
@@ -0,0 +1,42 @@
+﻿namespace FactoryPattern
+{
+    internal interface IProduct
+    {
+        void Operation();
+    }
+
+    class ConcreteProductA : IProduct
+    {
+        public void Operation() 
+        {
+            Console.WriteLine("Operation of ConcreteProductA");
+        }
+    }
+
+    class ConcreteProductB : IProduct
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operation of ConcreteProductB");
+        }
+    }
+
+    class ProductFactory
+    {
+        public IProduct CreateProduct(string type)
+        {
+            if(type == "A")
+            {
+                return new ConcreteProductA();
+            }
+            else if (type == "B")
+            {
+                return new ConcreteProductB();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid product type");
+            }
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using FactoryPattern;
+
+Console.WriteLine("Hello, World!");
+
+ProductFactory factory = new();
+
+IProduct productA = factory.CreateProduct("A");
+productA.Operation();
+
+
+IProduct productB = factory.CreateProduct("B");
+productB.Operation();
